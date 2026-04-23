83ab35b66edea9d2ea3ade3ea8257e92eecf991b
JosueMS <carlosmota1983m@gmail.com>
2026-04-22 21:16:23 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  // Esta sección ignora los errores de TypeScript durante la construcción
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Esta sección ignora los errores de ESLint durante la construcción
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
